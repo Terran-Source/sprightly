80b7f9b929cc1ee795a03c63b757176a34962c59
Tamal Patra <patra.tamal@gmail.com>
2020-05-18 20:44:29 +0530

    #22 - remove comments

--- src/sprightly/lib/features/settings/domain/repositories/setting_repositories.dart
+++ src/sprightly/lib/features/settings/domain/repositories/setting_repositories.dart
@@ -6,20 +6,7 @@ class SettingsRepo {
   final db.AppSettings _appSettings;
 
   SettingsRepo(this._appSettings);
-  // SettingsRepo(dao.SettingsDao dao, {String environment})
-  //     : _appSettings = db.AppSettings(dao, environment: environment);
 
   AppDetails appDetails() => AppDetails.fromDB(_appSettings);
   AppSettings appSettings() => AppSettings.fromDB(_appSettings);
 }
-// abstract class SettingsRepo {
-//   final db.AppSettings _appSettings;
-
-//   SettingsRepo(dao.SettingsDao dao, {String environment})
-//       : _appSettings = db.AppSettings(dao, environment: environment);
-
-//   Future<Either<FormattedException, AppDetails>> get appDetails =>
-//       DartzExecution.call(() => AppDetails.fromDB(_appSettings));
-//   Future<Either<FormattedException, AppSettings>> get appSettings =>
-//       DartzExecution.call(() => AppSettings.fromDB(_appSettings));
-// }
