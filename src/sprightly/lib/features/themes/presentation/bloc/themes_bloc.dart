931160963ed99494111215ad379e85ca4d32a77c
Tamal Patra <patra.tamal@gmail.com>
2021-01-07 19:18:07 +0530

    fixing bloc initialState

--- src/sprightly/lib/features/themes/presentation/bloc/themes_bloc.dart
+++ src/sprightly/lib/features/themes/presentation/bloc/themes_bloc.dart
@@ -2,15 +2,16 @@ library sprightly.feature.themes_bloc;
 
 import 'dart:async';
 
-import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 
 part 'themes_event.dart';
 part 'themes_state.dart';
 
 class ThemesBloc extends Bloc<ThemesEvent, ThemesState> {
-  @override
-  ThemesState get initialState => ThemesInitial();
+  ThemesBloc({ThemesState initialState})
+      : super(initialState ?? ThemesInitial());
+
   @override
   Stream<ThemesState> mapEventToState(
     ThemesEvent event,
